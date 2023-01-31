89f51a213414c2c6e545a7e30c1b5abdcbc7f768
Mark Karpeles <magicaltux@gmail.com>
2023-01-31 11:33:19 +0900

    add some comments

--- concat.go
+++ concat.go
@@ -2,6 +2,7 @@ package putil
 
 import "bytes"
 
+// Concat will attempt to put multiple values together if possible or return a map otherwise
 func Concat(obj ...any) any {
 	canString := true
 	for _, v := range obj {
--- i18n.go
+++ i18n.go
@@ -6,6 +6,7 @@ type i18nLookup interface {
 	LookupSimple(ctx context.Context, token string, pStr []string, noFilter bool) string
 }
 
+// LookupI18N will perform a i18n lookup in the current context if any is available, or return a @token value otherwise
 func LookupI18N(ctx context.Context, token string, pStr []string, noFilter bool) any {
 	if ctx != nil {
 		i18n, ok := ctx.Value("_i18n").(i18nLookup)
